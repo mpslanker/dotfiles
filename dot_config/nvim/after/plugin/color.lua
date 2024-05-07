8d55f6e2a763f05c84695c4161c45fea4ba78a9f
Matthew Slanker <ms@msitp.com>
2024-05-07 17:18:41 -0600

    changes to color.lua for nvim

--- dot_config/nvim/after/plugin/color.lua
+++ dot_config/nvim/after/plugin/color.lua
@@ -1,7 +1,8 @@
+vim.g.tokyonight_sytle = "moon"
 vim.g.tokyonight_transparent_sidebar = true
 vim.g.tokyonight_transparent = true
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.opt.background = "dark"
+-- vim.opt.background = "dark"
 
 vim.cmd("colorscheme tokyonight-night")
